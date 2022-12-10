1452bbbe0244ac0c026bbd24a1f805967f01a64c
devilzcore <aldosilva1@live.com>
2022-12-10 03:55:44 -0300

    Update method post ObterPorId

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -3,13 +3,13 @@ using TrilhaApiDesafio.Models;
 
 namespace TrilhaApiDesafio.Context
 {
-    public class OrganizadorContext : DbContext
+  public class OrganizadorContext : DbContext
+  {
+    public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
     {
-        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
-        {
-            
-        }
 
-        public DbSet<Tarefa> Tarefas { get; set; }
     }
+
+    public DbSet<Tarefa> Tarefas { get; set; }
+  }
 }
